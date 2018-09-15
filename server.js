e40ac38bb0360032200d0f805f202d5f5d8ee309
alpox <elias.be@gmx.ch>
2018-09-15 23:41:46 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.static("build"));
+
+app.listen(process.env.PORT, function() {
+  console.log(`Example app listening on port ${process.env.port}!`);
+});
